95cd9601756055dbd19399098900ea85448a4223
Emmanuel Ochoa <eorivera95@gmail.com>
2021-08-17 20:01:56 -0600

    making a function to capitalize the first letter of each word

---
+++ textUtilities.js
@@ -0,0 +1,15 @@
+let expect = require('chai').expect;
+
+expect(true).to.be.true;
+
+function titleCase(title){
+    let words = title.split(' ')
+    let titleCasedWords = words.map(word=>word[0].toUpperCase() +word.substring(1))
+
+    return titleCasedWords.join(' ')
+}
+
+expect(titleCase('the great mouse detective')).to.be.a('string')
+expect(titleCase('a')).to.equal(('A'))
+expect(titleCase('vertigo')).to.equal('Vertigo')
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective')
